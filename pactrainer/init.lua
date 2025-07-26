025d68590d142f6eb9204e7a7e47cab5fd6a642c
10yard <jon123wilson@hotmail.com>
2025-07-26 17:49:08 +0100

    update docs

--- pactrainer/init.lua
+++ pactrainer/init.lua
@@ -3,13 +3,15 @@
 --
 --The Pac-Man Trainer is an aid to learning the common patterns for completing boards.  
 --You follow an on-screen path through each board.  For the pattern to hold,  you must
---stay on track and turn the corners quickly.  The HUD shows you some information which
---can help you on your way during gameplay.  On the left of the screen you will see the
---current level (e.g. "L. 001"),  the current pattern (e.g. "P. 1/3"), and the current 
---status (e.g. S. ACE).  The status will change if you start falling behind to "OK" and 
---finally "BAD" based on the number of dropped movements/frames.  It is still possible 
---to complete the board with a "BAD" status but following the path will not guarantee
---success and you should be more cautious.
+--stay on track and turn the corners quickly.  
+--The HUD shows you some information which can help you on your way during gameplay.  
+--On the left of the screen you will see the current level (e.g. "L. 001"),  the current
+--pattern (e.g. "P. 1/3"), and the current status (e.g. S. ACE).  The status will change 
+--if you start falling behind to "OK" and finally "BAD" based on the number of dropped 
+--movements/frames.  It is still possible to complete the board with a "BAD" status but 
+--following the path will not guarantee success and you should be more cautious.  
+--If you die then the pattern will automatically fail and you will see a blinking "FAIL" 
+--message.  You are on your own to complete the board.
 --
 --Press P2 button to toggle between the currently available pattern sets.
 --    
